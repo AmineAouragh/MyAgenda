960eef5c6c9cd8d18345b70a300e66eb3f22db8f
Amine Aouragh <71039249+AmineAouragh@users.noreply.github.com>
2021-07-25 16:36:34 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,23 +16,6 @@ function App(props) {
     <div className="todoapp stack-large">
       <h1>My Agenda</h1>
 
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What do I have today?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
 
       <div className="filters btn-group stack-exception">
 
