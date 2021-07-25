8ed461b80a5735c5386b34dc9431e348141c7951
Amine Aouragh <71039249+AmineAouragh@users.noreply.github.com>
2021-07-25 22:51:17 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,28 @@ import ToDo from './components/ToDo'
 import Form from './components/Form'
 import FilterButton from './components/FilterButton'
 
+
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+}
+
+const FILTER_NAMES = Object.keys(FILTER_MAP)
+
 function App(props) {
 
   const [tasks, setTasks] = useState(props.tasks)
+  const [filter, setFilter] = useState('All')
+
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton 
+       key={name} 
+       name={name}
+       isPressed={name === filter} 
+       setFilter={setFilter}
+    />
+  ))
 
   const toggleTaskCompleted = id => {
 
@@ -49,7 +68,7 @@ function App(props) {
         key={task.id}
         toggleTaskCompleted={toggleTaskCompleted}
         deleteTask={deleteTask}
-        editTask={editedTask}
+        editTask={editTask}
       />
   ))
 
@@ -65,9 +84,7 @@ function App(props) {
 
       <div className="filters btn-group stack-exception">
 
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
 
       </div>
 
