93e41a237c823c6120903a15b11cef87a9c6a339
Amine Aouragh <71039249+AmineAouragh@users.noreply.github.com>
2021-07-26 16:56:55 +0200

    Update ToDo.js

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,9 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+
+const usePrevious = value => {
+    const ref = useRef()
+    useEffect(() => {
+      ref.current = value
+    })
+    return ref.current
+}
 
 export default function Todo(props) {
 
     const [isEditing, setEditing] = useState(false)
     const [newName, setNewName] = useState('')
+
+    const wasEditing = usePrevious(isEditing)
+
+    const editFieldRef = useRef(null)
+    const editButtonRef = useRef(null)
 
     const handleChange = e => {
       setNewName(e.target.value)
@@ -29,6 +42,7 @@ export default function Todo(props) {
                type="text" 
                value={newName}
                onChange={handleChange}
+               ref={editFieldRef}
             />
           </div>
 
@@ -71,6 +85,7 @@ export default function Todo(props) {
                  type="button" 
                  className="btn"
                  onClick={() => setEditing(true)}
+                 ref={editButtonRef}
               >
                 Edit <span className="visually-hidden">{props.name}</span>
               </button>
@@ -87,6 +102,15 @@ export default function Todo(props) {
 
         </div>
     )
+
+    useEffect(() => {
+      if (!wasEditing && isEditing) {
+        editFieldRef.current.focus()
+      }
+      if (wasEditing && !isEditing) {
+        editButtonRef.current.focus()
+      }
+    }, [wasEditing, isEditing])
 
     return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>
 }
