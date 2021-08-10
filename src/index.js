d8e695698dc30157311baa621c82e8e5a2fae9c4
Amine Aouragh <71039249+AmineAouragh@users.noreply.github.com>
2021-08-10 19:29:19 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import allStoredTasks from './App'
 
 const DATA = [
   { id: "todo-0", name: "Eat", completed: true },
@@ -9,7 +10,15 @@ const DATA = [
   { id: "todo-2", name: "Repeat", completed: false }
 ]
 
+
+const entries = Object.entries(localStorage)
+
+const obj = Object.fromEntries(entries)
+
+console.log(obj)
+
 ReactDOM.render(
   <App tasks={DATA} />,
   document.getElementById('root')
 )
+
